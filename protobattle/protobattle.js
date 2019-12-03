aba0c595c3748fe0853e4f7789ddb24b25088225
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2019-12-03 15:00:13 -0600

    Created basic connections in Protobattle folder

--- protobattle/protobattle.js
+++ protobattle/protobattle.js
@@ -1,26 +1,23 @@
-// function recieve(friendCode) {
 var lastPeerId = null;
 var peer = null; // Own peer object
 var peerId = null;
 var conn = null;
 var recvId = document.getElementById("receiver-id");
-var status = document.getElementById("status");
+var recvIdInput = document.getElementById("receiver-id-input");
+var connectButton = document.getElementById("connect-button");
+var stat = document.getElementById("stat");
 
 var message = document.getElementById("message");
 var sendMessageBox = document.getElementById("sendMessageBox");
 var sendButton = document.getElementById("sendButton");
 var clearMsgsButton = document.getElementById("clearMsgsButton");
-/**
- * Create the Peer object for our end of the connection.
- *
- * Sets up callbacks that handle any events related to our
- * peer object.
- */
+
 function initialize() {
     // Create own peer object with connection to shared PeerJS server
     peer = new Peer(null, {
         debug: 2
     });
+
     peer.on('open', function (id) {
         // Workaround for peer.reconnect deleting previous id
         if (peer.id === null) {
@@ -31,8 +28,9 @@ function initialize() {
         }
         console.log('ID: ' + peer.id);
         recvId.innerHTML = "ID: " + peer.id;
-        status.innerHTML = "Awaiting connection...";
+        stat.innerHTML = "Awaiting connection...";
     });
+
     peer.on('connection', function (c) {
         // Allow only a single connection
         if (conn) {
@@ -44,92 +42,91 @@ function initialize() {
         }
         conn = c;
         console.log("Connected to: " + conn.peer);
-        status.innerHTML = "Connected"
+        stat.innerHTML = "Connected"
         ready();
     });
+
     peer.on('disconnected', function () {
-        status.innerHTML = "Connection lost. Please reconnect";
+        stat.innerHTML = "Connection lost. Please reconnect";
         console.log('Connection lost. Please reconnect');
         // Workaround for peer.reconnect deleting previous id
         peer.id = lastPeerId;
         peer._lastServerId = lastPeerId;
         peer.reconnect();
     });
+
     peer.on('close', function () {
         conn = null;
-        status.innerHTML = "Connection destroyed. Please refresh";
+        stat.innerHTML = "Connection destroyed. Please refresh";
         console.log('Connection destroyed');
     });
+
     peer.on('error', function (err) {
         console.log(err);
         alert('' + err);
     });
-    // };
+}
 
+/**
+* Create the connection between the two Peers.
+*
+* Sets up callbacks that handle any events related to the
+* connection and data received on it.
+*/
+function join() {
+    // Close old connection
+    if (conn) {
+        conn.close();
+    }
+    // Create connection to destination peer specified in the input field
+    conn = peer.connect(recvIdInput.value, {
+        reliable: true
+    });
 
-    /**
- * Triggered once a connection has been achieved.
- * Defines callbacks to handle incoming data and connection events.
+    conn.on('open', function () {
+        stat.innerHTML = "Connected to: " + conn.peer;
+        console.log("Connected to: " + conn.peer);
+        // Check URL params for comamnds that should be sent immediately
+        var command = getUrlParam("command");
+        if (command)
+            conn.send(command);
+    });
+
+    // // Handle incoming data (messages only since this is the signal sender)
+    // conn.on('data', function (data) {
+    //     addMessage("<span class=\"peerMsg\">Peer:</span> " + data);
+    // });
+
+    conn.on('close', function () {
+        stat.innerHTML = "Connection closed";
+    });
+};
+
+/**
+ * Get first "GET style" parameter from href.
+ * This enables delivering an initial command upon page load.
+ *
+ * Would have been easier to use location.hash.
  */
+function getUrlParam(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(window.location.href);
+    if (results == null)
+        return null;
+    else
+        return results[1];
+};
+
+function recieve(friendCode) {
 
 }
 
-// function send(friendCode) {
+function send(friendCode) {
 
-//     var lastPeerId = null;
-//     var peer = null; // own peer object
-//     var conn = null;
-//     var recvIdInput = document.getElementById("receiver-id");
-//     var status = document.getElementById("status");
-//     var message = document.getElementById("message");
-//     var goButton = document.getElementById("goButton");
-//     var resetButton = document.getElementById("resetButton");
-//     var fadeButton = document.getElementById("fadeButton");
-//     var offButton = document.getElementById("offButton");
-//     var sendMessageBox = document.getElementById("sendMessageBox");
-//     var sendButton = document.getElementById("sendButton");
-//     var clearMsgsButton = document.getElementById("clearMsgsButton");
-//     var connectButton = document.getElementById("connect-button");
-//     var cueString = "<span class=\"cueMsg\">Cue: </span>";
-//     /**
-//      * Create the Peer object for our end of the connection.
-//      *
-//      * Sets up callbacks that handle any events related to our
-//      * peer object.
-//      */
-//     function initialize() {
-//         // Create own peer object with connection to shared PeerJS server
-//         peer = new Peer(null, {
-//             debug: 2
-//         });
-//         peer.on('open', function (id) {
-//             // Workaround for peer.reconnect deleting previous id
-//             if (peer.id === null) {
-//                 console.log('Received null id from peer open');
-//                 peer.id = lastPeerId;
-//             } else {
-//                 lastPeerId = peer.id;
-//             }
-//             console.log('ID: ' + peer.id);
-//         });
-//         peer.on('disconnected', function () {
-//             status.innerHTML = "Connection lost. Please reconnect";
-//             console.log('Connection lost. Please reconnect');
-//             // Workaround for peer.reconnect deleting previous id
-//             peer.id = lastPeerId;
-//             peer._lastServerId = lastPeerId;
-//             peer.reconnect();
-//         });
-//         peer.on('close', function () {
-//             conn = null;
-//             status.innerHTML = "Connection destroyed. Please refresh";
-//             console.log('Connection destroyed');
-//         });
-//         peer.on('error', function (err) {
-//             console.log(err);
-//             alert('' + err);
-//         });
-//     };
-// }
+}
+
+connectButton.addEventListener('click', join);
 
 initialize();
