551f79a5167161df3c74b7bb0d1ccdc11a504ce0
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2019-12-05 14:53:30 -0600

    Added fadeout upon succeful connection retrieval

--- connect.js
+++ connect.js
@@ -54,6 +54,8 @@ function initialize() {
         conn = c;
         console.log("Connected to: " + conn.peer);
         stat.innerHTML = "Connected"
+        loginModal.style.opacity = 0;
+        setTimeout(removeModal, 2000); //Wait two seconds before removing modal for animation to finish
         ready();
     });
 
@@ -97,7 +99,7 @@ function join() {
     conn.on('open', function () {
         stat.innerHTML = "Connected to: " + conn.peer;
         console.log("Connected to: " + conn.peer);
-        
+
         // Fade out modal
         loginModal.style.opacity = 0;
         setTimeout(removeModal, 2000); //Wait two seconds before removing modal for animation to finish
