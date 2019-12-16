fd56735f8e488c299fcb480dbe3b3f5bc9a85fac
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2019-12-16 16:06:57 -0600

    Added two new monsters
    Cherrim - Grass Tank
    Luxio - Fast Electirc Attacker

--- assets/monster-library.js
+++ assets/monster-library.js
@@ -9,7 +9,7 @@ let monsterLibraryDB =
                 50,     //AT
                 50,     //DF
                 100,    //AC
-                0,    //EV
+                0,       //EV
                 5       //SP
             ],
             "moves": [
@@ -64,7 +64,7 @@ let monsterLibraryDB =
                 50,     //AT
                 50,     //DF
                 100,    //AC
-                0,    //EV
+                0,       //EV
                 5       //SP
             ],
             "moves": [
@@ -107,6 +107,116 @@ let monsterLibraryDB =
                         "heal": 50
                     },
                     "limit": 1
+                }
+            ]
+        },
+        {
+            "name": "Luxio",
+            "type": "Electric",
+            "id": 404,
+            "stats": [
+                100,    //HP
+                50,     //AT
+                45,     //DF
+                100,    //AC
+                0,       //EV
+                6       //SP
+            ],
+            "moves": [
+                {
+                    "name": "Tackle",
+                    "type": 0,
+                    "base-power": 40,
+                    "base-accuracy": 100,
+                    "effect": null,
+                    "limit": null
+                },
+                {
+                    "name": "Growl",
+                    "type": 1,
+                    "base-power": 0,
+                    "base-accuracy": 100,
+                    "effect": {
+                        "stat": "AT",
+                        "value": "-10"
+                    },
+                    "limit": null
+                },
+                {
+                    "name": "Spark",
+                    "type": 2,
+                    "base-power": 30,
+                    "base-accuracy": 90,
+                    "effect": {
+                        "status": "Paralyze",
+                        "chance": 40
+                    },
+                    "limit": null
+                },
+                {
+                    "name": "Potion",
+                    "type": 4,
+                    "base-power": 0,
+                    "base-accuracy": 0,
+                    "effect": {
+                        "heal": 50
+                    },
+                    "limit": 1
+                }
+            ]
+        },
+        {
+            "name": "Cherrim",
+            "type": "Grass",
+            "id": 421,
+            "stats": [
+                100,    //HP
+                40,     //AT
+                60,     //DF
+                100,    //AC
+                0,      //EV
+                4       //SP
+            ],
+            "moves": [
+                {
+                    "name": "Tackle",
+                    "type": 0,
+                    "base-power": 40,
+                    "base-accuracy": 100,
+                    "effect": null,
+                    "limit": null
+                },
+                {
+                    "name": "Worry Seed",
+                    "type": 1,
+                    "base-power": 0,
+                    "base-accuracy": 100,
+                    "effect": {
+                        "stat": "AC",
+                        "value": "-10"
+                    },
+                    "limit": null
+                },
+                {
+                    "name": "Solar Beam",
+                    "type": 0,
+                    "base-power": 65,
+                    "base-accuracy": 45,
+                    "effect": {
+                        "status": null,
+                        "chance": null
+                    },
+                    "limit": null
+                },
+                {
+                    "name": "Sub-Potions",
+                    "type": 4,
+                    "base-power": 0,
+                    "base-accuracy": 0,
+                    "effect": {
+                        "heal": 30
+                    },
+                    "limit": 3
                 }
             ]
         }
